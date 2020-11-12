fd871e786f2e674251a217d4a1e15b39459d5eb9
+ <barboradujcikova@gmail.com>
2020-11-12 19:48:06 +0100

    +-

--- Open-Lab-10.01/Program.cs
+++ Open-Lab-10.01/Program.cs
@@ -6,7 +6,28 @@ namespace Open_Lab_10._01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            
+            Book LOTR = new Book();
+            
+            LOTR.SetBook("Creative name of book", 100);
+            
+            Console.ReadKey();
+        }
+    }
+    class Book
+    {
+        public string title;
+        
+        private int pages;
+        
+        public void SetBook(string title, int pages)
+        {
+            this.title = title;
+            
+            this.pages = pages;
+            
+            Console.WriteLine(String.Format($"Book \"{title}\" has {pages} pages."));
         }
     }
 }
